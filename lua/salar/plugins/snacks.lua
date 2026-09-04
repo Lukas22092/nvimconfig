84d8966edfeab2712c8553fc02c462fad70767ab
Lukas <173559986+Lukas22092@users.noreply.github.com>
2026-09-04 17:50:31 +0200

    Disable snacks picker (conflicts with Telescope), fix rndr pcall

--- lua/salar/plugins/snacks.lua
+++ lua/salar/plugins/snacks.lua
@@ -7,6 +7,7 @@ return {
 				border = "rounded",
 			},
 		},
+		picker = { enabled = false },
 	},
 	keys = {
 		{
--- lua/salar/plugins/telescope.lua
+++ lua/salar/plugins/telescope.lua
@@ -9,7 +9,6 @@ return {
 	config = function()
 		local telescope = require("telescope")
 		local actions = require("telescope.actions")
-		local rndr = require("rndr")
 
 		local rg_args = {
 			"rg",
@@ -23,27 +22,32 @@ return {
 			".git",
 		}
 
-		telescope.setup({
-			defaults = {
-				vimgrep_arguments = rg_args,
-				buffer_previewer_maker = rndr.telescope_buffer_previewer_maker,
-				file_ignore_patterns = {
-					"node_modules",
-					"vendor",
-					"build",
-					"%.git",
-				},
-				path_display = { "smart" },
-				mappings = {
-					i = {
-						["<C-k>"] = actions.move_selection_previous,
-						["<C-j>"] = actions.move_selection_next,
-						["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
-						["<C-t>"] = require("trouble.sources.telescope").open,
-						["<C-e>"] = actions.to_fuzzy_refine,
-					},
+		local defaults = {
+			vimgrep_arguments = rg_args,
+			file_ignore_patterns = {
+				"node_modules",
+				"vendor",
+				"build",
+				"%.git",
+			},
+			path_display = { "smart" },
+			mappings = {
+				i = {
+					["<C-k>"] = actions.move_selection_previous,
+					["<C-j>"] = actions.move_selection_next,
+					["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+					["<C-e>"] = actions.to_fuzzy_refine,
 				},
 			},
+		}
+
+		local ok_rndr, rndr = pcall(require, "rndr")
+		if ok_rndr and rndr.telescope_buffer_previewer_maker then
+			defaults.buffer_previewer_maker = rndr.telescope_buffer_previewer_maker
+		end
+
+		telescope.setup({
+			defaults = defaults,
 			pickers = {
 				live_grep = {
 					additional_args = function()
@@ -65,9 +69,7 @@ return {
 		keymap.set("n", "<leader>fw", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
 		keymap.set("n", "<leader>sg", "<cmd>Telescope live_grep<cr>", { desc = "Grep (ripgrep)" })
 		keymap.set("n", "<leader>fs", "<cmd>Telescope lsp_document_symbols<cr>", { desc = "Find document symbols" })
-		keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", {
-			desc = "Find string under cursor in cwd",
-		})
+		keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
 		keymap.set("n", "<leader>fk", "<cmd>Telescope keymaps<cr>", { desc = "Find keymaps" })
 	end,
 }
