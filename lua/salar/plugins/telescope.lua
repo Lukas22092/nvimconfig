4c9f6d2398f768bee7f820e611a67f8fe7177d62
Lukas <173559986+Lukas22092@users.noreply.github.com>
2026-09-04 17:53:50 +0200

    Remove rndr dependency from telescope config, clean setup

--- lua/salar/plugins/telescope.lua
+++ lua/salar/plugins/telescope.lua
@@ -10,53 +10,47 @@ return {
 		local telescope = require("telescope")
 		local actions = require("telescope.actions")
 
-		local rg_args = {
-			"rg",
-			"--color=never",
-			"--no-heading",
-			"--with-filename",
-			"--line-number",
-			"--column",
-			"--smart-case",
-			"!",
-			".git",
-		}
-
-		local defaults = {
-			vimgrep_arguments = rg_args,
-			file_ignore_patterns = {
-				"node_modules",
-				"vendor",
-				"build",
-				"%.git",
-			},
-			path_display = { "smart" },
-			mappings = {
-				i = {
-					["<C-k>"] = actions.move_selection_previous,
-					["<C-j>"] = actions.move_selection_next,
-					["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
-					["<C-e>"] = actions.to_fuzzy_refine,
+		telescope.setup({
+			defaults = {
+				file_ignore_patterns = {
+					"node_modules",
+					"vendor",
+					"build",
+					"%.git",
+				},
+				path_display = { "smart" },
+				mappings = {
+					i = {
+						["<C-k>"] = actions.move_selection_previous,
+						["<C-j>"] = actions.move_selection_next,
+						["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+						["<C-e>"] = actions.to_fuzzy_refine,
+					},
 				},
 			},
-		}
-
-		local ok_rndr, rndr = pcall(require, "rndr")
-		if ok_rndr and rndr.telescope_buffer_previewer_maker then
-			defaults.buffer_previewer_maker = rndr.telescope_buffer_previewer_maker
-		end
-
-		telescope.setup({
-			defaults = defaults,
 			pickers = {
 				live_grep = {
 					additional_args = function()
-						return rg_args
+						return {
+							"--color=never",
+							"--no-heading",
+							"--with-filename",
+							"--line-number",
+							"--column",
+							"--smart-case",
+						}
 					end,
 				},
 				grep_string = {
 					additional_args = function()
-						return rg_args
+						return {
+							"--color=never",
+							"--no-heading",
+							"--with-filename",
+							"--line-number",
+							"--column",
+							"--smart-case",
+						}
 					end,
 				},
 			},
@@ -66,7 +60,7 @@ return {
 
 		local keymap = vim.keymap
 		keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files" })
-		keymap.set("n", "<leader>fw", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
+		keymap.set("n", "<leader>fw", "<cmd>Telescope live_grep<cr>", { desc = "Grep (ripgrep)" })
 		keymap.set("n", "<leader>sg", "<cmd>Telescope live_grep<cr>", { desc = "Grep (ripgrep)" })
 		keymap.set("n", "<leader>fs", "<cmd>Telescope lsp_document_symbols<cr>", { desc = "Find document symbols" })
 		keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
