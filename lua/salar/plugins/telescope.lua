f7b136e6af49fe5a90fbab18472c80c9c3b52dc7
Lukas <173559986+Lukas22092@users.noreply.github.com>
2026-09-04 18:37:30 +0200

    Fix ft_to_lang crash, telescope nil results, and incomplete transparent background

--- lua/salar/plugins/telescope.lua
+++ lua/salar/plugins/telescope.lua
@@ -1,6 +1,6 @@
 return {
 	"nvim-telescope/telescope.nvim",
-	branch = "0.1.x",
+	branch = "master",
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
@@ -10,8 +10,19 @@ return {
 	config = function()
 		local telescope = require("telescope")
 		local actions = require("telescope.actions")
-		local lga_actions = require("telescope-live-grep-args.actions")
-		local trouble = require("trouble.sources.telescope")
+
+		local function try_require(mod)
+			local ok, m = pcall(require, mod)
+			return ok and m or nil
+		end
+
+		local lga_actions = try_require("telescope-live-grep-args.actions")
+		local trouble_module = try_require("trouble.sources.telescope")
+
+		local trouble_key = nil
+		if trouble_module then
+			trouble_key = trouble_module.open
+		end
 
 		telescope.setup({
 			defaults = {
@@ -38,49 +49,50 @@ return {
 						["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
 						["<C-s>"] = actions.send_to_qflist + actions.open_qflist,
 						["<C-e>"] = actions.to_fuzzy_refine,
-						["<C-t>"] = trouble.open,
 					},
 					n = {
 						["<C-s>"] = actions.send_to_qflist + actions.open_qflist,
-						["<C-t>"] = trouble.open,
 						["dd"] = function(prompt_bufnr)
-							require("telescope.actions").delete_buffer(prompt_bufnr)
+							actions.delete_buffer(prompt_bufnr)
 						end,
-					},
-				},
-			},
-			pickers = {
-				live_grep = {
-					mappings = {
-						i = {
-							["<C-f>"] = lga_actions.quote_prompt(),
-							["<C-a>"] = lga_actions.quote_prompt({ postfix = " --hidden " }),
-						},
-					},
-				},
-			},
-			extensions = {
-				live_grep_args = {
-					auto_quoting = true,
-					mappings = {
-						i = {
-							["<C-f>"] = lga_actions.quote_prompt(),
-							["<C-a>"] = lga_actions.quote_prompt({ postfix = " --hidden " }),
-						},
 					},
 				},
 			},
 		})
 
-		telescope.load_extension("fzf")
-		telescope.load_extension("live_grep_args")
+		if trouble_key then
+			telescope.setup({
+				defaults = {
+					mappings = {
+						i = { ["<C-t>"] = trouble_key },
+						n = { ["<C-t>"] = trouble_key },
+					},
+				},
+			})
+		end
+
+		local ok_fzf, _ = pcall(telescope.load_extension, "fzf")
+		if not ok_fzf then
+			vim.notify("Telescope fzf extension failed to load", vim.log.levels.WARN)
+		end
+
+		local ok_lga, _ = pcall(telescope.load_extension, "live_grep_args")
+		if not ok_lga then
+			vim.notify("Telescope live_grep_args extension failed to load", vim.log.levels.WARN)
+		end
 
 		local keymap = vim.keymap
 		keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files" })
 		keymap.set("n", "<leader>fw", "<cmd>Telescope live_grep<cr>", { desc = "Grep (ripgrep)" })
-		keymap.set("n", "<leader>sg", function()
-			require("telescope").extensions.live_grep_args.live_grep_args()
-		end, { desc = "Grep with args (ripgrep flags: -g, -t, etc.)" })
+
+		if ok_lga then
+			keymap.set("n", "<leader>sg", function()
+				require("telescope").extensions.live_grep_args.live_grep_args()
+			end, { desc = "Grep with args (ripgrep flags: -g, -t, etc.)" })
+		else
+			keymap.set("n", "<leader>sg", "<cmd>Telescope live_grep<cr>", { desc = "Grep (live_grep_args unavailable)" })
+		end
+
 		keymap.set("n", "<leader>fs", "<cmd>Telescope lsp_document_symbols<cr>", { desc = "Find document symbols" })
 		keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
 		keymap.set("n", "<leader>fk", "<cmd>Telescope keymaps<cr>", { desc = "Find keymaps" })
@@ -89,11 +101,15 @@ return {
 		keymap.set("n", "<leader>sr", "<cmd>Telescope registers<cr>", { desc = "Registers" })
 		keymap.set("n", "<leader>sh", "<cmd>Telescope help_tags<cr>", { desc = "Help tags" })
 
-		-- Send Telescope results to Spectre for inline editing
 		vim.api.nvim_create_user_command("SpectreFromTelescope", function()
-			local qf = vim.fn.getqflist({ title = 0, items = 0 })
+			local spectre_ok, spectre = pcall(require, "spectre")
+			if not spectre_ok then
+				vim.notify("spectre.nvim not installed", vim.log.levels.ERROR)
+				return
+			end
+			local qf = vim.fn.getqflist({ items = 0 })
 			if #qf.items > 0 then
-				require("spectre").open_qflist(qf.items)
+				spectre.open_qflist(qf.items)
 			else
 				vim.notify("No quickfix items to send to Spectre", vim.log.levels.WARN)
 			end
